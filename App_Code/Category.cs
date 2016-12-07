1981043d2b8be62f788d09efa46743a606f3941e
heoconxz <heoconxz1995@gmail.com>
2016-12-06 20:09:41 -0600

    DataSet

--- App_Code/Category.cs
+++ App_Code/Category.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Ecommerce;
-
 /// <summary>
 /// Summary description for Category
 /// </summary>
 public static class Category
 {
-   
- 
+    private static DataSetTableAdapters.CategoryTableAdapter ds = new DataSetTableAdapters.CategoryTableAdapter();
+    public static void list() {
+         foreach(DataSet.CategoryRow row in ds.GetData())
+        {
+            Console.WriteLine(row.Name);
+        }
+    }
 }
